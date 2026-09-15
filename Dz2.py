71aba785803c706712116ed8b2a7eb8a7e233192
Egorist-R <egorov.e.v@list.ru>
2026-09-15 22:25:03 +0900

    Add files via upload

--- Dz2.py
+++ Dz2.py
@@ -1,6 +1,7 @@
-etap1 = input()
-etap2 = input()
-etap3 = input()
-etap4 = input()
+a = input()
+b = input()
+c = input()
+d = input()
 
-print(etap1, etap2, etap3, etap4, sep="=>")
+print(a, b, c, d, sep="=>")
+
